d57c5f6a64a662530777bdffa0c52e5e0fc18784
aidannairn <aidan.nairn95@gmail.com>
2019-11-28 16:54:50 +1300

    Create user schema. Allow user to sign up with jwt.

---
+++ pages/api/signup.js
@@ -0,0 +1,35 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+import connectDb from '../../utils/connectDb'
+import User from '../../models/User'
+
+connectDb()
+
+export default async (req, res) => {
+  const { name, email, password } = req.body
+  try {
+    // 1) Check to see if the user already exists in the database.
+    const user = await User.findOne({ email })
+    if (user) {
+      return res.status(422).send(`User already exists with email ${email}.`)
+    }
+    // 2) If new user, hash their password.
+    const hash = await bcrypt.hash(password, 10)
+    // 3) Create new user.
+    const newUser = await new User({
+      name,
+      email,
+      password: hash
+    }).save()
+    // 4) Create token for the new user.
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d'
+    })
+    // 5) Send back token.
+    res.status(201).json(token)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error signing up user. Please try again later.')
+  }
+}

6d19338f33b9db4a06a1f82d32a285b9c8f335ef
aidannairn <aidan.nairn95@gmail.com>
2019-11-29 15:00:57 +1300

    Universal logout using LocalStorage.

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import Layout from '../components/_App/Layout'
 import { redirectUser } from '../utils/auth'
 import baseUrl from '../utils/baseUrl'
 import axios from 'axios'
+import Router from 'next/router'
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -46,6 +47,17 @@ class MyApp extends App {
       }
     }
     return { pageProps }
+  }
+
+  componentDidMount() {
+    window.addEventListener('storage', this.syncLogout)
+  }
+
+  syncLogout = event => {
+    if (event.key === 'logout') {
+      console.log('Logged out from storage.')
+      Router.push('/login')
+    }
   }
 
   render() {

d522171495d475062ed9a90976fae7c9e09d2694
aidannairn <aidan.nairn95@gmail.com>
2019-12-02 17:30:46 +1300

    Checkout customer carts with Stripe.

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
 import { Button, Segment, Divider } from 'semantic-ui-react'
 
 import calculateCartTotal from '../../utils/calculateCartTotal'
 
-function CartSummary({ products }) {
+function CartSummary({ products, handleCheckout, success }) {
   const [cartAmount, setCartAmount] = React.useState(0)
   const [stripeAmount, setStripeAmount] = React.useState(0)
   const [isCartEmpty, setCartEmpty] = React.useState(false)
@@ -20,13 +21,26 @@ function CartSummary({ products }) {
       <Divider />
       <Segment clearing size="large">
         <strong>Sub total:</strong> ${cartAmount}
-        <Button
-          icon="cart"
-          disabled={isCartEmpty}
-          color="teal"
-          floated="right"
-          content="Checkout"
-        />
+        <StripeCheckout
+          name="React Reserve"
+          amount={stripeAmount}
+          image={products.length > 0 ? products[0].product.mediaUrl : ''}
+          currency="USD"
+          shippingAddress={true}
+          billingAddress={true}
+          zipCode={true}
+          token={handleCheckout}
+          triggerEvent="onClick"
+          stripeKey="pk_test_Zly5mLAEe5bjhS7XmVw8EXaD00UVYGUBfx"
+        >
+          <Button
+            icon="cart"
+            disabled={isCartEmpty || success}
+            color="teal"
+            floated="right"
+            content="Checkout"
+          />
+        </StripeCheckout>
       </Segment>
     </>
   )
--- pages/cart.js
+++ pages/cart.js
@@ -7,9 +7,12 @@ import cookie from 'js-cookie'
 import CartItemList from '../components/Cart/CartItemList'
 import CartSummary from '../components/Cart/CartSummary'
 import baseUrl from '../utils/baseUrl'
+import catchErrors from '../utils/catchErrors'
 
 function Cart({ products, user }) {
   const [cartProducts, setCartProducts] = React.useState(products)
+  const [success, setSuccess] = React.useState(false)
+  const [loading, setLoading] = React.useState(false)
 
   async function handleRemoveFromCart(productId) {
     const url = `${baseUrl}/api/cart`
@@ -22,14 +25,35 @@ function Cart({ products, user }) {
     setCartProducts(response.data)
   }
 
+  async function handleCheckout(paymentData) {
+    try {
+      setLoading(true)
+      const url = `${baseUrl}/api/checkout`
+      const token = cookie.get('token')
+      const payload = { paymentData }
+      const headers = { headers: { Authorization: token } }
+      await axios.post(url, payload, headers)
+      setSuccess(true)
+    } catch (error) {
+      catchErrors(error, window.alert)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
-    <Segment>
+    <Segment loading={loading}>
       <CartItemList
         user={user}
         products={cartProducts}
         handleRemoveFromCart={handleRemoveFromCart}
+        success={success}
       />
-      <CartSummary products={cartProducts} />
+      <CartSummary
+        products={cartProducts}
+        handleCheckout={handleCheckout}
+        success={success}
+      />
     </Segment>
   )
 }

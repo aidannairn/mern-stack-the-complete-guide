c6d5c13049dd5edebe8aecff98096cd2474710dc
aidannairn <aidan.nairn95@gmail.com>
2019-12-03 15:47:24 +1300

    Create account page and display order history.

--- pages/account.js
+++ pages/account.js
@@ -1,5 +1,28 @@
-function Account() {
-  return <>account</>;
+import { parseCookies } from 'nookies'
+import axios from 'axios'
+
+import AccountHeader from '../components/Account/AccountHeader'
+import AccountOrders from '../components/Account/AccountOrders'
+import baseUrl from '../utils/baseUrl'
+
+function Account({ user, orders }) {
+  return (
+    <>
+      <AccountHeader {...user} />
+      <AccountOrders orders={orders} />
+    </>
+  )
 }
 
-export default Account;
+Account.getInitialProps = async ctx => {
+  const { token } = parseCookies(ctx)
+  if (!token) {
+    return { orders: [] }
+  }
+  const payload = { headers: { Authorization: token } }
+  const url = `${baseUrl}/api/orders`
+  const response = await axios.get(url, payload)
+  return response.data
+}
+
+export default Account
